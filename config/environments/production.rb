4d64d24322acd4c35f92432a49ae685fa644b0d1
Adam Booth <4rlm@protonmail.ch>
2017-08-06 12:20:24 -0500

    fully cloned version.  duplicated postgresql db and contents too.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'bds-max.herokuapp.com' }
+  
+  ### Need to Uncomment Below, and change the heroku app name!!!
+  # config.action_mailer.default_url_options = { host: 'bds-max.herokuapp.com' }
 end
